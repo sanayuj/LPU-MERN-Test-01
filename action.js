31d65e0b23ae543f224652f4d577970401bd2f44
sanayuj <sanayuj2255@gmail.com>
2026-02-04 09:58:38 +0530

    update

---
+++ action.js
@@ -0,0 +1,12 @@
+function random(){
+    let emojies=["😀","😃","😁","✋","☕️","⏰","🤓","🥲","📖","🔥","🫙","😶‍🌫️","😶","🫶","👨‍⚕️","👀"]
+    let length=emojies.length
+    
+    let result=""
+    let randomValue=Math.floor(Math.random()*emojies.length)
+    let picker=document.getElementsByClassName("emoji_display")
+    picker.
+    
+}
+
+random()
